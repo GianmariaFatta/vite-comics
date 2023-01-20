aa95f48f5b1db7e1f3ae2b9dcc4e58ced9bd3f44
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-01-20 18:40:29 +0100

    First step header commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <script>
+import AppHeader from './AppHeader.vue';
 export default {
-
+  components: {
+    AppHeader: AppHeader,
+  }
 }
 </script>
 
 <template>
+  <header>
+    <AppHeader />
+  </header>
+  <main></main>
+  <footer></footer>
 
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
 
+body {
+  background-color: #1C1C1C;
+}
+
+.container {
+  max-width: 1048px;
+  margin: 0 auto;
+  border: 2px dashed darkred
+}
+
+header {
+  background-color: #fff;
+  width: 100%;
+  height: 110px;
+
+
+
+}
+
+footer {
+  background-color: #303030;
+}
 </style>
---
+++ src/AppHeader.vue
@@ -0,0 +1,145 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            pages: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-left">
+
+                <img src="../public/img/dc-logo.png" alt="">
+
+            </div>
+            <div class="col-right">
+                <ul>
+                    <li v-for="page in pages " :class="page.current"><a :href="page.url"></a>{{ page.text }}</li>
+                </ul>
+            </div>
+        </div>
+        <div class="jumbo">
+            <a href="#">
+                <h3>-->Content goes here </h3>
+            </a>
+        </div>
+    </div>
+
+</template>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+img {
+
+    max-width: 100%;
+    padding: 5px;
+
+}
+
+.container {
+    max-width: 1048;
+}
+
+.col-left {
+    width: 40%;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    vertical-align: middle;
+    line-height: 150px;
+}
+
+.col-right {
+    width: 60%;
+
+}
+
+.col-right ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style-type: none;
+    cursor: pointer;
+    gap: 10px;
+}
+
+.row {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.jumbo {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    height: 100px;
+
+}
+
+.jumbo a {
+    color: #fff;
+    text-decoration: none;
+}
+</style>
